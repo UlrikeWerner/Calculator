f0957dc697d13914d4e1f76f5778641936a6b902
Ulrike Werner <ulrike_werner@web.de>
2022-05-11 22:42:00 +0200

    implement calculator functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,49 @@
 // TODO: import things from other modules here
+import { setOutput } from "./output.js";
+import { add, subtract, multiply, divide } from "./math.js";
+import { input } from "./input.js";
+import { getRandomNumber } from "./random.js";
 
 const handleCalculate = (event) => {
-  // TODO: use event.target to figure out the operation, then calculate and output the result
+  const operation = event.target.getAttribute("data-calculate");
+  let operator;
+  let result;
+  const a = Number.parseInt(input.a.value);
+  const b = Number.parseInt(input.b.value);
+
+  switch (operation) {
+    case "add":
+      result = add(a, b);
+      operator = "+";
+      break;
+    case "subtract":
+      result = subtract(a, b);
+      operator = "-";
+      break;
+    case "multiply":
+      result = multiply(a, b);
+      operator = "*";
+      break;
+    case "divide":
+      result = divide(a, b);
+      operator = "/";
+      break;
+  }
+  setOutput({ a: a, b: b, operator: operator, result: result });
 };
 
 const handleRandom = (event) => {
-  // TODO: use event.target to decide which inputs value to set to a random number
+  if (event.target.getAttribute("data-random-for-input") === "a") {
+    input.a.value = getRandomNumber();
+  } else {
+    input.b.value = getRandomNumber();
+  }
 };
 
-document.querySelectorAll('[data-calculate]').forEach((buttonCalculate) => {
-  buttonCalculate.addEventListener('click', handleCalculate);
+document.querySelectorAll("[data-calculate]").forEach((buttonCalculate) => {
+  buttonCalculate.addEventListener("click", handleCalculate);
 });
 
-document.querySelectorAll('[data-random-for-input]').forEach((buttonRandom) => {
-  buttonRandom.addEventListener('click', handleRandom);
+document.querySelectorAll("[data-random-for-input]").forEach((buttonRandom) => {
+  buttonRandom.addEventListener("click", handleRandom);
 });
--- src/output.js
+++ src/output.js
@@ -1,10 +1,15 @@
 const output = {
-  a: document.querySelector('#output-a'),
-  b: document.querySelector('#output-b'),
-  operator: document.querySelector('#output-operator'),
-  result: document.querySelector('#output-result'),
+  a: document.querySelector("#output-a"),
+  b: document.querySelector("#output-b"),
+  operator: document.querySelector("#output-operator"),
+  result: document.querySelector("#output-result"),
 };
 
-export const setOutput = ({ a = 0, b = 0, operator = '+', result = 0 }) => {
-  // TODO: output the parameters to the DOM
+export const setOutput = ({ a = 0, b = 0, operator = "+", result = 0 }) => {
+  output.a.textContent = a;
+  output.b.textContent = b;
+  output.operator.textContent = operator;
+  output.result.textContent = result.toLocaleString(undefined, {
+    maximumFractionDigits: 2,
+  });
 };
